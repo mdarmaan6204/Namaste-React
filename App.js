a285ea8df34219da96acb155828f1347f59c1e3c
Md Armaan <mdarmaan6204@gmail.com>
2023-11-26 10:39:11 +0530

    Episode - 01

---
+++ App.js
@@ -0,0 +1,17 @@
+const parent = React.createElement("div" , 
+{id : "parent"} ,
+[React.createElement("div" , {id : "child1"} ,
+[React.createElement("h1" , {} , "This is H1 Tag" ) , 
+React.createElement("h2" , {} , "This is H2 Tag" )] ) ,
+React.createElement("div" , {id : "child1"} ,
+[React.createElement("h1" , {} , "This is H1 Tag" ) , 
+React.createElement("h2" , {} , "This is H2 Tag" )] )]
+ );
+
+ console.log(parent);  
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
+
